30cdefc7778cd88a13644ada8c873c2518381eea
chalanaK95 <ChalanaK@fortude.co>
2022-08-03 23:51:21 +0530

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
 
 const app = express();
+dotenv.config()
 const port = process.env.PORT || 5000
 
+const connect = () => {
+    mongoose.connect(process.env.MONGO_URL).then(() => {
+        console.log("Coneccted to DB");
+    }).catch((err) => {
+        console.log(err);
+    })
+}
+
 app.listen(port, () => {
+    connect()
     console.log(`Server connected to ${port}`);
 })
