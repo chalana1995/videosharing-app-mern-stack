61ee6a56afa72bf5a23eed35468756e54bac4dae
chalanaK95 <ChalanaK@fortude.co>
2022-08-04 12:21:13 +0530

    create route files

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/users.js'
+import commentRouter from './routes/comments.js'
+import videoRouter from './routes/videoes.js'
+import authRouter from './routes/auth.js'
+
 
 
 const app = express();
 dotenv.config()
 const port = process.env.PORT || 5000
+
+app.use('/api/auth', authRouter)
+app.use('/api/users', userRouter)
+app.use('/api/comments', commentRouter)
+app.use('/api/videos', videoRouter)
+
 
 const connect = () => {
     mongoose.connect(process.env.MONGO_URL).then(() => {
