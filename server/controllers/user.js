70e4752088302487fe8d8aa6f5b929439346ad5e
chalanaK95 <ChalanaK@fortude.co>
2022-08-04 23:49:28 +0530

    subscribe part done

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -48,11 +48,35 @@ export const getUser = async (req, res, next) => {
 }
 
 export const subscribe = async (req, res, next) => {
+    try {
+        await User.findById(req.user.id, {
+            $push: { subscribedUsers: req.params.id }
+        })
 
+        await User.findByIdAndUpdate(req.params.id, {
+            $inc: { subscribers: 1 }
+        })
+
+        res.status(200).json("Subscription successfull.")
+    } catch (error) {
+        next(error)
+    }
 }
 
 export const unSubscribe = async (req, res, next) => {
+    try {
+        await User.findById(req.user.id, {
+            $pull: { subscribedUsers: req.params.id }
+        })
 
+        await User.findByIdAndUpdate(req.params.id, {
+            $inc: { subscribers: -1 }
+        })
+
+        res.status(200).json("Unsubscription successfull.")
+    } catch (error) {
+        next(error)
+    }
 }
 
 export const like = async (req, res, next) => {
