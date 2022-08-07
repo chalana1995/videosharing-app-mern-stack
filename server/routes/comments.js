274077bf10334596fca5a9a6aa4cf7cb0f52687f
chalanaK95 <ChalanaK@fortude.co>
2022-08-07 19:35:34 +0530

    comment crud done

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,8 +1,14 @@
 import express from 'express'
+import { addComment, deleteComment, getComments } from '../controllers/comment.js';
+import { verifyToken } from '../verifyToken.js';
 
 
 const router = express.Router();
 
+
+router.post("/", verifyToken, addComment)
+router.get("/", getComments)
+router.delete("/:videoId", verifyToken, deleteComment)
 
 
 
